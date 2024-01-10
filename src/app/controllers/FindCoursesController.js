8d98d2039453cdfd8532640771875122a7dd498a
CaduSilva28 <edu420092@gmail.com>
2024-01-09 23:01:23 -0300

    Alterando o retorno da rota de cursos

--- src/app/controllers/FindCoursesController.js
+++ src/app/controllers/FindCoursesController.js
@@ -2,10 +2,15 @@ import jsonCourses from '../database/courses.json';
 
 class findCoursesController {
     async findCourses(req, res) {
+        const data = {
+            "lstCourses": jsonCourses, 
+            "stgData": "Análise e Desenvolvimento de Sistemas, Ciências Contábeis, Administração e Pedagogia"
+        };
+
         try{
             res.status(200).json({
                 status: true,
-                data: jsonCourses
+                data
             });
         }catch(error){
             res.status(500).json({
